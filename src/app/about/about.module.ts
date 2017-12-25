8c411c1ea9f5ba3ff89aaeb658de5537d0650a0d
xiaomo <xiaomo@xiaomo.info>
2017-12-25 09:10:30 +0800

    修复溢出错误

--- src/app/about/about.module.ts
+++ src/app/about/about.module.ts
@@ -3,6 +3,12 @@ import {CommonModule} from '@angular/common';
 import {RouterModule} from '@angular/router';
 import {AboutComponent} from './about.component';
 
+
+/**
+ * https://github.com/angular/angular-cli/issues/6309
+ * 修复maxSize错误
+ */
+
 @NgModule({
   imports: [
     RouterModule.forChild([
