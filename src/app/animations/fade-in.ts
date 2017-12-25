381217f64b3cf4ae2b0b3136898b7f55655c92a5
xiaomo <xiaomo@xiaomo.info>
2017-12-25 15:40:23 +0800

    format code

--- src/app/animations/fade-in.ts
+++ src/app/animations/fade-in.ts
@@ -1,11 +1,11 @@
-import { trigger, style, transition, animate } from '@angular/animations';
+import {animate, style, transition, trigger} from '@angular/animations';
 
 export const fadeIn = trigger('fadeIn', [
-  transition("void => *", [
-    style({ opacity: 0 }),
-    animate(600, style({ opacity: 1 }))
+  transition('void => *', [
+    style({opacity: 0}),
+    animate(600, style({opacity: 1}))
   ]),
-  transition("* => void", [
-    animate(600, style({ opacity: 0 }))
+  transition('* => void', [
+    animate(600, style({opacity: 0}))
   ])
 ]);
